c977c2cd73f89402c64ab99d76e2b195b79acde4
Aaron Krumsieg <a.krumsieg@gmail.com>
2021-05-25 14:03:01 -0500

    Added Update and Delete endpoints to RestaurantController

--- RestaurantRaterAPI/Controllers/RestaurantController.cs
+++ RestaurantRaterAPI/Controllers/RestaurantController.cs
@@ -51,7 +51,44 @@ namespace RestaurantRaterAPI.Controllers
         }
 
         //Update(PUT)
+        [HttpPut]
+        public async Task<IHttpActionResult> UpdateRestaurant(int id, Restaurant updatedRestaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                Restaurant restaurant = await _context.Restaurants.FindAsync(id);
+                if(restaurant != null)
+                {
+                    restaurant.Name = updatedRestaurant.Name;
+                    restaurant.Address = updatedRestaurant.Address;
+                    restaurant.Rating = updatedRestaurant.Rating;
+
+                    await _context.SaveChangesAsync();
+                    return Ok();
+                }
+                return NotFound();
+            }
+            return BadRequest(ModelState);
+        }
 
         //Delete
+        [HttpDelete]
+        public async Task<IHttpActionResult> DeleteRestaurant (int id)
+        {
+            Restaurant restaurant = await _context.Restaurants.FindAsync(id);
+            if (restaurant == null) //different check logic
+            {
+                return NotFound();
+            }
+
+            _context.Restaurants.Remove(restaurant);
+
+            if (await _context.SaveChangesAsync() == 1) //if one db entry was changed
+            {
+                return Ok("The restaurant was successfully deleted.");
+            }
+
+            return InternalServerError();
+        }
     }
 }
