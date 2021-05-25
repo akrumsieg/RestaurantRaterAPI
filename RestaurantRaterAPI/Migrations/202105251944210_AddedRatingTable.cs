ba14314ae388c956f15f0c37a117e7c86053cc30
Aaron Krumsieg <a.krumsieg@gmail.com>
2021-05-25 15:12:23 -0500

    Added Rating entity and Db set, added a migration, and fixed reference loop error

---
+++ RestaurantRaterAPI/Migrations/202105251944210_AddedRatingTable.cs
@@ -0,0 +1,35 @@
+﻿namespace RestaurantRaterAPI.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedRatingTable : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Ratings",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        RestaurantID = c.Int(nullable: false),
+                        FoodScore = c.Double(nullable: false),
+                        EnvironmentScore = c.Double(nullable: false),
+                        CleanlinessScore = c.Double(nullable: false),
+                    })
+                .PrimaryKey(t => t.ID)
+                .ForeignKey("dbo.Restaurants", t => t.RestaurantID, cascadeDelete: true)
+                .Index(t => t.RestaurantID);
+            
+            DropColumn("dbo.Restaurants", "Rating");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Restaurants", "Rating", c => c.Double(nullable: false));
+            DropForeignKey("dbo.Ratings", "RestaurantID", "dbo.Restaurants");
+            DropIndex("dbo.Ratings", new[] { "RestaurantID" });
+            DropTable("dbo.Ratings");
+        }
+    }
+}
--- RestaurantRaterAPI/Models/Restaurant.cs
+++ RestaurantRaterAPI/Models/Restaurant.cs
@@ -10,13 +10,28 @@ namespace RestaurantRaterAPI.Models
     {
         [Key]
         public int ID { get; set; }
+
         [Required]
         public string Name { get; set; }
+
         [Required]
         public string Address { get; set; }
-        [Required]
-        public double Rating { get; set; }
 
-        public bool IsRecommended => Rating > 3.5;
+        public virtual List<Rating> Ratings { get; set; } = new List<Rating>();
+
+        public double Rating
+        {
+            get
+            {
+                double totalAverageRating = 0;
+                foreach (Rating rating in Ratings)
+                {
+                    totalAverageRating += rating.AverageRating;
+                }
+                return totalAverageRating / Ratings.Count();
+            }
+        }
+
+        public bool IsRecommended => Rating > 8.5;
     }
 }
