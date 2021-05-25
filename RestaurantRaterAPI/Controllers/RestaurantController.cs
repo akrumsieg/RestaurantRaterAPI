c29874a84439bf348fc3346693b10e0534e56f62
Aaron Krumsieg <a.krumsieg@gmail.com>
2021-05-25 12:14:02 -0500

    Added Restaurant class and controller. Added migrations.

---
+++ RestaurantRaterAPI/Controllers/RestaurantController.cs
@@ -0,0 +1,31 @@
+﻿using RestaurantRaterAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace RestaurantRaterAPI.Controllers
+{
+    public class RestaurantController : ApiController
+    {
+        private RestaurantDbContext _context = new RestaurantDbContext();
+        public async Task<IHttpActionResult> PostRestaurant(Restaurant model)
+        {
+            if (model == null)
+            {
+                return BadRequest("Your request body cannot be empty");
+            }
+            if (ModelState.IsValid) //checks if all required properties are included
+            {
+                _context.Restaurants.Add(model); //stages the add
+                await _context.SaveChangesAsync(); //saves changes
+
+                return Ok(); //http 200 message
+            }
+            return BadRequest(ModelState); //generates message saying ModelState is invalid
+        }
+    }
+}
